222f4e7b2c99514eac5dfbb39e50e5cb057ba449
samuelho-dev <samuelho343@gmail.com>
2023-06-25 11:18:31 -0700

    fix search map to return non null

--- datascrape.py
+++ datascrape.py
@@ -5,15 +5,16 @@ import requests
 def industry_list():
     page = requests.get("https://companiesmarketcap.com/all-categories/")
     soup = BeautifulSoup(page.content, "html.parser")
-    list = soup.find("tbody")
-    table_list = list.find_all("td")
+    body_list = soup.find("tbody")
+    table_list = body_list.find_all("td")
     
     def title_search(el) : 
         search = el.find('a')
-        if search: 
-            return el.get("data-sort")
-            
-    result = list(map(lambda el: title_search(el), table_list))
+        if search and search is not None: 
+            data = el.get("data-sort")
+            return data
+        
+    result = [title_search(el) for el in table_list if title_search(el) is not None]
         
     return result
 
@@ -27,7 +28,9 @@ async def top_companies(industry):
         company_name = el.select_one("div.company-name").text.strip()
         company_code = el.select_one("div.company-code").text.strip()
         return {"name" : company_name, "code": company_code}
+    
     result = list(map(lambda el: extract_company(el), table_list))
         
     return result
 
+# lambda <arguments> : <Return Value if condition is True> if <condition> else <Return Value if condition is False>
