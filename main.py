4a45a67f57635839d9d74ef4857509d313344601
samuelho-dev <samuelho343@gmail.com>
2023-06-29 17:29:08 -0700

    add news api

--- main.py
+++ main.py
@@ -15,25 +15,25 @@ class Data(BaseModel):
 app = FastAPI()
 
 @app.get('/')
-def fetch():
-    print('hello')
+def fetch_industries():
     return datascrape.industry_list()
 
-@app.get("/{industry}")
-async def fetch_data(industry : str, data : Data):
-
-    date_from = data.date_from
-    date_to = data.date_to
+@app.get("/industry/{industry}")
+def fetch_data(industry : str, url: str, date_from : str, date_to : str):
     
     if not industry or not date_from or not date_to :
-        return ({"error": "No data provided"}), 400
+        return {"error": "No data provided"}, 400
     
-    top_companies = datascrape.top_companies(industry)
-    updated_companies = list(map(
-        lambda company : yf.Ticker(company["code"]).info,
-        top_companies
-    ))
-    
+    top_companies = datascrape.top_companies(url)
+
+    updated_companies = []
+    for company in top_companies:
+        try:
+            info = yf.Ticker(company["code"]).info
+            updated_companies.append(info)
+        except Exception as e:
+            print(f"Error getting info for company {company['code']}: {e}")
+
     news_info = news.news_info(updated_companies, industry, date_from, date_to)
     news_prompt = infoprompt.news_prompt(news_info, industry)
     market_prompt = infoprompt.info_prompt(updated_companies, industry)
--- news.py
+++ news.py
@@ -1,32 +1,29 @@
-import requests
-from dotenv import load_dotenv
+from newsapi import NewsApiClient
 import os
 
+newsapi = NewsApiClient(os.getenv("NEWS_API_KEY"))
 
 def news_info(updatedCompanies, industry, date_from, date_to):
     data = []
-    API_KEY = os.getenv("NEWS_API_KEY")
-    
     for company in updatedCompanies:
         symbol = company["symbol"]
         name = company["longName"]
         query = f"{industry} industry {symbol} {name}"
-        URL = "https://newsapi.org/v2/everything"
         PARAMS = {
             "q": query,
             "from": date_from,
             "to": date_to,
-            "pageSize": 2,
+            "page": 2,
             "sortBy": "relevancy",
-            "apiKey": API_KEY,
         }
-        request = requests.get(url=URL, params=PARAMS)
+        request = newsapi.get_everything(PARAMS)
+        print(request)
         articles = request.json()["articles"]
         results = request.json()["totalResults"]
         if results > 0:
             for article in articles:
                 obj = {
-                    "sourceName": article.source.name,
+                    "sourceName": article["source"]["name"],
                     "author": article["author"],
                     "title": article["title"],
                     "description": article["description"],
